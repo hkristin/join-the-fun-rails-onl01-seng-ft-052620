30659c44378af9b0d37862a10d3deb62692f4375
Kristin Howard <hkristin@andrew.cmu.edu>
2020-08-07 23:52:09 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140212204456) do
+ActiveRecord::Schema.define(version: 20140212205242) do
+
+  create_table "rides", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "taxis", force: :cascade do |t|
     t.datetime "created_at", null: false
